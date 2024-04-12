65c0ad27ce3a4acf8ed7ea83ddb1b468c6505c53
nimranaz148 <nimranaz148@gmail.com>
2024-04-12 08:34:56 -0800

    first commit

---
+++ index.ts
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+let c_vlaue : {[key : string] : number }= {
+    PKR :278.40,
+    UAE :3.67,
+    USD :1 
+}
+
+let ans = await inquirer.prompt([{
+    type : "list",
+    name : "from",
+    message : "Converting from",
+    choices : ['PKR','UAE','USD']
+},
+
+{
+type : "list",
+name : "to",
+message : "Converting to",
+choices : ['PKR','UAE','USD']
+},
+
+{
+    type : "number",
+    name : "amount",
+    message : "Your amount to convert"
+}
+
+])
+console.log((c_vlaue[ans.to] / c_vlaue[ans.from] * ans.amount).toFixed(2));
